01d5985f11bc077241f51b8babcc6656e92795f2
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2025-07-24 22:59:25 -0600

    Add Tailwind CSS base directives to main stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import AuthProvider from './context/AuthContext';
 import { BrowserRouter} from 'react-router-dom';
 import App from './App';
+import './styles/App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
