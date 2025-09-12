a7b1a5ba64f747682a8e37e45604f9dfc190656f
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2025-09-11 22:42:34 -0600

    add chakra provider to wrap app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import './styles/index.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
-
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
